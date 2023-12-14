0fd497761e281521a02ca6eb544436fec5276cb0
MinaEhab77 <fouadattia09@gmail.com>
2023-12-13 17:04:28 -0800

    wb3deen

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "main.h"
+ 
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENTS 10
 
 /**
  * main - Entry point of the program
@@ -13,16 +16,17 @@
  */
 int main(void)
 {
-int status;
+int arg_count, status;
 char *input = NULL;
 size_t input_size = 0;
+char *token, *args[MAX_ARGUMENTS];
+pid_t pid;
 while (printf("$ "), getline(&input, &input_size, stdin) != -1)
 {
 size_t length = strlen(input);
 if (input[length - 1] == '\n')
 input[length - 1] = '\0';
-char *token, *args[MAX_ARGUMENTS];
-int arg_count = 0;
+arg_count = 0;
 for (token = strtok(input, " "); token && arg_count < MAX_ARGUMENTS;
 token = strtok(NULL, " "))
 args[arg_count++] = token;
@@ -38,14 +42,15 @@ else if (!cce(args[0]))
 fprintf(stderr, "Command not found: %s\n", args[0]);
 continue;
 }
-pid_t pid = fork();
-if {
-	(pid < 0) perror("Fork failed"), exit(EXIT_FAILURE);
-}
-else if
+pid = fork();
+if (pid < 0)
 {
-	(pid == 0) execvp(args[0], args)
-	perror("Error executing command"
+	perror("Fork failed"), exit(EXIT_FAILURE);
+}
+else if (pid == 0) 
+{
+	execvp(args[0], args);
+	perror("Error executing command");
 	exit(EXIT_FAILURE);
 }
 else
--- pe.c
+++ pe.c
@@ -1,4 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "main.h"
+
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENTS 10
 /**
  * pe - current environment variables.
  */
